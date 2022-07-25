e1100b2a358c47f51b7613481911b648fcc0c281
masterjyc <masterjinyc@163.com>
2022-07-25 21:32:59 +0800

    2-6 add Hello World interface 访问地址：http://localhost:8080/hello

--- src/main/java/com/mixedmodel/system/config/PaperApplication.java
+++ src/main/java/com/mixedmodel/system/config/PaperApplication.java
@@ -4,11 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.mixedmodel")
 @SpringBootApplication
 public class PaperApplication {
-
 
     private static final Logger LOG = LoggerFactory.getLogger(PaperApplication.class);
 
---
+++ src/main/java/com/mixedmodel/system/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.mixedmodel.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jyc
+ * @date 2022/7/25 - 20:54
+ */
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
