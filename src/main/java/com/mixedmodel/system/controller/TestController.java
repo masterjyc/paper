90f3cc2a2ca2df4297f6a4ae8a603b6f50ae526c
masterjyc <masterjinyc@163.com>
2022-07-25 22:10:11 +0800

    2-7 modify Hello 接口测试test.http

--- src/main/java/com/mixedmodel/system/controller/TestController.java
+++ src/main/java/com/mixedmodel/system/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.mixedmodel.system.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping ("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World!" + name;
     }
 }
