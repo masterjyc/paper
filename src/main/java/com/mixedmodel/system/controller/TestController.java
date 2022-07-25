08eed7f6d588e31bc3fe0d6626a17c5e07f78c42
masterjyc <masterjinyc@163.com>
2022-07-25 22:31:59 +0800

    2-8 增加自定义配置项

--- src/main/java/com/mixedmodel/system/controller/TestController.java
+++ src/main/java/com/mixedmodel/system/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.mixedmodel.system.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    String testProperties;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testProperties;
     }
 
     @PostMapping ("/hello/post")
