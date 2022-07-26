c89f915e0ff3ab6d378cebfb5815d6b7c6752578
masterjyc <masterjinyc@163.com>
2022-07-26 22:56:07 +0800

    3-6 新增ebook表，生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -1,3 +1,27 @@
+#电子书表
+drop table if exists `ebook`;
+create table `ebook`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    `category1_id` bigint comment '分类1',
+    `category2_id` bigint comment '分类2',
+    `description` varchar(200) comment '描述',
+    `cover` varchar(200) comment '封面',
+    `doc_count` int comment '文档数',
+    `view_count` int comment '阅读数',
+    `vote_count` int comment '点赞数',
+    primary key (`id`)
+)engine=innodb default charset=utf8mb4 comment='电子书';
+
+insert into `ebook` (id,name,description) values(1,'SpringBoot 入门','零基础入门 Java 开发');
+
+insert into `ebook` (id,name,description) values(2,'Vue 入门','零基础入门 Vue 开发');
+
+insert into `ebook` (id,name,description) values(3,'Python 入门','零基础入门 Python 开发');
+
+
+
+
 drop table if exists `test`;
 create table `test`(
     `id` bigint not null comment 'id',
